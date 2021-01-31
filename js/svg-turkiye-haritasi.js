c2569bf7787775ddd5ed68f8925d324b848a09e6
Batuhan Hasan Akin <batuhanhasanakin@gmail.com>
2021-01-31 18:35:35 +0300

    change link from meetup to gdgcommunity

--- js/svg-turkiye-haritasi.js
+++ js/svg-turkiye-haritasi.js
@@ -1,55 +1,71 @@
 /*! SVG Türkiye Haritası | MIT Lisans | dnomak.com */
 
 function svgturkiyeharitasi() {
-  const gdg ={"adana":"GDG-Adana","ankara":"GDGAnkara","antalya":"GDGAntalya","bolu":"GDGBolu","burdur":"gdgburdur","bursa":"GDGBursa","denizli":"GDGDenizli","edirne":"GDGEdirne","eskisehir":"gdgeskisehir","hatay":"gdghatay","isparta":"GdgIsparta","mersin":"GDG-Mersin","istanbul":"GDGIstanbul","izmir":"GDG-Izmir","kayseri":"GDG-Kayseri","kocaeli":"GDGKocaeli","konya":"GDGKonya","kutahya":"GDG-Kutahya", "manisa":"GDG-Manisa", "samsun":"GDG-Samsun", "tekirdag":"gdgtekirdag", "trabzon":"GDGTrabzon", "sivas":"GDGSivas", "sanliurfa":"GDG-Sanliurfa", "duzce":"GDG-Duzce-Meetup" }
-  const element = document.querySelector('#svg-turkiye-haritasi');
-  const info = document.querySelector('.il-isimleri');
-  const imge = document.querySelector('.imge');
+  const gdg = {
+    adana: "GDG-Adana",
+    ankara: "GDG-Ankara",
+    antalya: "GDG-Antalya",
+    bolu: "GDG-Bolu",
+    burdur: "gdg-burdur",
+    bursa: "GDG-Bursa",
+    denizli: "GDG-Denizli",
+    edirne: "GDG-Edirne",
+    eskisehir: "gdg-eskisehir",
+    hatay: "gdg-hatay",
+    isparta: "Gdg-Isparta",
+    mersin: "GDG-Mersin",
+    istanbul: "GDG-Istanbul",
+    izmir: "GDG-Izmir",
+    kayseri: "GDG-Kayseri",
+    kocaeli: "GDG-Kocaeli",
+    konya: "GDG-Konya",
+    kutahya: "GDG-Kutahya",
+    manisa: "GDG-Manisa",
+    samsun: "GDG-Samsun",
+    tekirdag: "gdg-tekirdag",
+    trabzon: "GDG-Trabzon",
+    sivas: "GDG-Sivas",
+    sanliurfa: "GDG-Sanliurfa",
+    duzce: "GDG-Duzce",
+  };
+  const element = document.querySelector("#svg-turkiye-haritasi");
+  const info = document.querySelector(".il-isimleri");
+  const imge = document.querySelector(".imge");
 
-  element.addEventListener(
-    'mouseover',
-    function (event) {
-      if (event.target.tagName === 'path' && event.target.parentNode.id !== 'guney-kibris' && event.target.parentNode.className.baseVal === 'gdg') {
-        imge.src ='./gdg-logo.png';
-        info.innerHTML = [
-          '<div>',
-          event.target.parentNode.getAttribute('data-iladi'),
-          '</div>',
-        ].join('');
-      }
+  element.addEventListener("mouseover", function (event) {
+    if (
+      event.target.tagName === "path" &&
+      event.target.parentNode.id !== "guney-kibris" &&
+      event.target.parentNode.className.baseVal === "gdg"
+    ) {
+      imge.src = "./gdg-logo.png";
+      info.innerHTML = [
+        "<div>",
+        event.target.parentNode.getAttribute("data-iladi"),
+        "</div>",
+      ].join("");
     }
-  );
-  element.addEventListener(
-    'mousemove',
-    function (event) {
-      imge.style.top = event.pageY + 15 + 'px';
-      imge.style.left = event.pageX + 10 + 'px';
-      info.style.top = event.pageY + 25 + 'px';
-      info.style.left = event.pageX +20+ 'px';
-    }
-  );
+  });
+  element.addEventListener("mousemove", function (event) {
+    imge.style.top = event.pageY + 15 + "px";
+    imge.style.left = event.pageX + 10 + "px";
+    info.style.top = event.pageY + 25 + "px";
+    info.style.left = event.pageX + 20 + "px";
+  });
 
-  element.addEventListener(
-    'mouseout',
-    function (event) {
-      imge.src = '';
-      info.innerHTML = '';
-    }
-  );
+  element.addEventListener("mouseout", function (event) {
+    imge.src = "";
+    info.innerHTML = "";
+  });
 
-  element.addEventListener(
-    'click',
-    function (event) {
-      if (event.target.tagName === 'path' && event.target.parentNode.className.baseVal === 'gdg') {
-        const parent = event.target.parentNode;
-        const id = parent.getAttribute('id');
-        
-        console.log(gdg[id])
-        window.location.href = (
-          'https://www.meetup.com/'
-          + gdg[id]
-        );
-      }
+  element.addEventListener("click", function (event) {
+    if (
+      event.target.tagName === "path" &&
+      event.target.parentNode.className.baseVal === "gdg"
+    ) {
+      const parent = event.target.parentNode;
+      const id = parent.getAttribute("id");
+      window.location.href = "https://gdg.community.dev/" + gdg[id];
     }
-  );
+  });
 }
